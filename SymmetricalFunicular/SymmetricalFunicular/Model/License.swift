e08924b9d7286996e2f2b14dc8f1e33250461441
Martin Mungai <mungaimrtn@gmail.com>
2024-01-17 14:12:20 +0300

    🛠️ setup models

---
+++ SymmetricalFunicular/SymmetricalFunicular/Model/License.swift
@@ -0,0 +1,24 @@
+//
+//  License.swift
+//  SymmetricalFunicular
+//
+//  Created by Martin Mungai on 17/01/2024.
+//
+
+import Foundation
+
+// MARK: - License
+struct License: Codable {
+    let key: String
+    let name: String
+    let spdxID: String
+    let url: String
+    let nodeID: String
+
+    enum CodingKeys: String, CodingKey {
+        case key, name
+        case spdxID = "spdx_id"
+        case url
+        case nodeID = "node_id"
+    }
+}
